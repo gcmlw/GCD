cae71770da9963dcb233cf3db143ec0354e8dc54
gcmlw <62331772+gcmlw@users.noreply.github.com>
2020-05-24 09:54:50 +0800

    Additional comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,4 @@
-# Assignment:
-# You should create one R script called run_analysis.R that does the following.
-# Merges the training and the test sets to create one data set.
-# Extracts only the measurements on the mean and standard deviation for each measurement.
-# Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive variable names.
-# From the data set in step 4, creates a second, independent tidy data set with the 
-# average of each variable for each activity and each subject.
-
-#write.table() using row.name=FALSE
+library(dplyr)
 
 # Read data
 testx = read.table("./data2/test/X_test.txt", sep = "", header = FALSE, stringsAsFactors = FALSE)
@@ -62,5 +53,6 @@ subdata <- alldata[,fcollist]
 shortcollist <- collist[fcollist3]
 subdata2 <- subdata %>% group_by(subdata$subject,subdata$activity) %>% summarise_at(shortcollist, mean, na.rm=TRUE)
 
-write.table(subdata, "./data2/measurements.txt", row.names = FALSE)
+# write out data as table
+# write.table(subdata, "./data2/measurements.txt", row.names = FALSE)
 write.table(subdata2, "./data2/groupby.txt", row.names = FALSE)
